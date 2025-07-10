57112692594c0cebd93021e6e885022f2622d6a2
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-10 15:05:34 +0900

    Feat: DOM css 선택자 활용하여 접근

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // console.log(document);
+
+// DOM 직접 조회를 위한, html 구조 검색
 // console.dir(document);
 
+// DOM 직접 조회
 // document.body.children[1].children[0].href = "https://google.com";
 
+// id만 가능
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = "https://google.com";
 
-
+// css 선택자면 모두 가능
+document.querySelector('#external-link');
+anchorElement.href = "https://google.com";
