aae6628962371057870831214754167451b1274f
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-11 12:17:05 +0900

    Feat: js에서 DOM으로 요소 추가하기

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ anchorElement.href = "https://google.com";
 // css 선택자면 모두 가능
 document.querySelector('#external-link');
 anchorElement.href = "https://google.com";
+
+// js에서 DOM으로 요소 추가하기
+// 1. 요소 생성
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = "https://naver.com";
+newAnchorElement.textContent = "This leads to Naver!";
+
+// 2. 부모 요소에 접근
+let firstParagraph = document.querySelector('p');
+
+// 3. 부모 요소에 요소 삽입
+firstParagraph.appendChild(newAnchorElement);
